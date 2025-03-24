73f35151bf36f92312b803316a7177d94e19b0a9
Celine Mia Hoffmann Hansen <90177110+celi5451@users.noreply.github.com>
2025-03-24 09:21:38 +0100

    project client

---
+++ project-client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './index.html',  // Update this with your actual HTML file paths
+      './src/**/*.{html,js}', // If you have specific JS or other files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
